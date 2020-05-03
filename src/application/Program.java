073265383ac1de7a874398dc33f01a2bf3bf61fd
João Vilela <jppinhovilela@hotmail.com>
2020-05-03 12:13:36 -0300

    Bloco Try Catch

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,28 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner (System.in);
+		
+		try {
+			String[] vect = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			System.out.println(vect[position]);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Invalid position");
+		} catch (InputMismatchException e) {
+			System.out.println("Input error");
+		}
+		
+		
+		System.out.println("Fim do Programa");
+		
+		sc.close();
 
 	}
 
